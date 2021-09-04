cd672ed903ca006fd9f55031c2d1322304ecc175
nbloch <nblochn@gmail.com>
2021-09-04 21:29:31 +0300

    add blocks approach

---
+++ utils/blocks.py
@@ -0,0 +1,24 @@
+import cv2
+
+def decompose(image):
+    blocks = []
+    for i in range(4):
+        for j in range(4):
+            block = image[i * 50:i * 50 + 50, j * 40: j * 40 + 40]
+            blocks.append(block)
+    return blocks
+
+def compose(images_array):
+    slices = []
+    for i in range(4):
+        image1 = cv2.hconcat([images_array[4 * i], images_array[4 * i + 1]])
+        image2 = cv2.hconcat([images_array[4 * i + 2], images_array[4 * i + 3]])
+
+        slice_i = cv2.hconcat([image1, image2])
+        slices.append(slice_i)
+
+    half1 = cv2.vconcat([slices[0], slices[1]])
+    half2 = cv2.vconcat([slices[2], slices[3]])
+
+    image = cv2.vconcat([half1, half2])
+    return image
