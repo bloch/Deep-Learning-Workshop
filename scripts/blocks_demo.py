2c12a3aadec031c3891f886a1852619501a66ae5
nbloch <nblochn@gmail.com>
2021-09-08 10:43:19 +0300

    block demo

--- scripts/blocks_demo.py
+++ scripts/blocks_demo.py
@@ -7,11 +7,8 @@ from utils.blocks import decompose
 
 # This script takes a random fullsize frame and saves a grid of 16 blocks of frames.
 
-NUM_OF_FRAMES = len(os.listdir(TRAINING_SET_DIR))
-random_frame_index = random.randrange(NUM_OF_FRAMES)
-
-image_path = TRAINING_SET_DIR + "\\frame" + str(random_frame_index) + ".jpg"
-blocks_image_path = "blocks_frame" + str(random_frame_index) + ".jpg"
+image_path = os.path.join("..\demo_images", "frame6.jpg")
+blocks_image_path = "blocks_frame6.jpg"
 
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
